b9222a87281317c634849f9208fdb4dd03adc54d
Alec Figueroa <aleclfig@yahoo.com>
2025-04-01 20:53:37 +0000

    Service and Controller new classes - methods

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -2,6 +2,12 @@ package Controller;
 
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+import Service.AccountService;
+import Service.MessageService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import Model.Account;
+import Model.Message;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
@@ -9,6 +15,16 @@ import io.javalin.http.Context;
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
 public class SocialMediaController {
+
+    AccountService accountService;
+    MessageService messageService;
+
+    SocialMediaController()
+    {
+        accountService = new AccountService();
+        messageService = new MessageService();
+    }
+
     /**
      * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
      * suite must receive a Javalin object from this method.
@@ -17,6 +33,7 @@ public class SocialMediaController {
     public Javalin startAPI() {
         Javalin app = Javalin.create();
         app.get("example-endpoint", this::exampleHandler);
+        app.post("/register", this::postAccountHandler);
 
         return app;
     }
@@ -27,6 +44,23 @@ public class SocialMediaController {
      */
     private void exampleHandler(Context context) {
         context.json("sample text");
+    }
+
+    private void postAccountHandler(Context context) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        Account account = mapper.readValue(context.body(), Account.class);
+        Account registeredAccount = accountService.addAccount(account);
+        if (registeredAccount == null || registeredAccount.getUsername().length() < 4 || registeredAccount.getPassword().isEmpty())
+        {
+            context.status(400);
+        }
+        else if (accountService.getAccountUsernameById(account.getAccount_id()) != null)
+        {
+            context.status(400);
+        }
+        else 
+            context.status(200);
     }
 
 
---
+++ src/main/java/Service/AccountService.java
@@ -0,0 +1,16 @@
+package Service;
+import Model.Account;
+
+public class AccountService {
+    
+
+    public Account addAccount(Account account)
+    {
+        return null;
+    }
+
+    public String getAccountUsernameById(int account_id)
+    {
+        return null;
+    }
+}
